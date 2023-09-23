b1aaa4026c726151051dfefea583717635298c4d
王昱烨 <42343279+1621391916@users.noreply.github.com>
2023-09-23 14:38:30 +0000

    1

--- esports_helper_init.sh
+++ esports_helper_init.sh
@@ -1,10 +1,8 @@
 cd /root
 sudo apt update 
-sudo apt install git -y
-sudo apt install python3-pip -y
-sudo apt install vim -y
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt install -y git python3-pip vim screen
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /root/google-chrome-stable_current_amd64.deb
+sudo dpkg -i /root/google-chrome-stable_current_amd64.deb
 apt --fix-broken install -y
 git clone https://github.com/Yudaotor/EsportsHelper.git
 cd EsportsHelper
